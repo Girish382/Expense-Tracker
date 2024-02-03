2b37dc6eeb45a001665bd69ef046be2dd60bcf1b
Girish382 <girish382002@gmail.com>
2024-02-03 18:48:31 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
